14c836d1e5b654a71fdfc30409a50576f2533803
m78hayata <m78hayata@gmail.com>
2024-04-03 01:07:48 +0900

    add calculation function

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        num1 = request.form.get('num1')
+        num2 = request.form.get('num2')
+        result = int(num1) + int(num2)
+        return render_template('index.html', result=result)
+    return render_template('index.html', result=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
